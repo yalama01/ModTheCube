451f2d758ea6b4fa8f76b6ff9030a162ac3e113e
yalama01 <67355562+yalama01@users.noreply.github.com>
2022-01-04 03:27:32 -0500

    mod the cube source project

---
+++ ModTheCube/Assets/Spripts/Cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 0.4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 10.0f * Time.deltaTime, 10.0f * Time.deltaTime);
+
+        transform.Translate(0,0,0);
+    }
+}
---
+++ ModTheCube/Assets/Spripts/manager.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void Newcollor()
+    {
+        SceneManager.LoadScene("Scene");
+    }
+}
